27f98f2359a5770a85e41d9b75d74aa1f2dcadbe
Uto <daffamadeo@gmail.com>
2022-07-22 15:52:45 +0700

    Initial Commit

--- nano2/AppDelegate.swift
+++ nano2/AppDelegate.swift
@@ -6,15 +6,19 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import FirebaseStorage
+import FirebaseAuth
 import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- nano2/ViewController.swift
+++ nano2/ViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Join us today in our fun and games!"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    let momoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "momo"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .yellow
+    
+        view.addSubview(momoImageView)
+        view.addSubview(descriptionTextView)
+        
+        configure()
+    }
+    
+    func configure(){
+        momoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        momoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        momoImageView.widthAnchor.constraint(equalToConstant:200).isActive = true
+        momoImageView.heightAnchor.constraint(equalToConstant:200).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: momoImageView.bottomAnchor, constant: 50).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
 
 
